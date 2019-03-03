91b36653db059ad09cc24c7ab0dc692f6fc0f032
andrejlevkovitch <andrejlevkovitch@gmail.com>
2019-03-03 15:27:41 +0300

    change project tree

--- build.cmake
+++ build.cmake
@@ -1,5 +1,7 @@
 # build.cmake
 
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
 IF(NOT CMAKE_BUILD_TYPE)
   SET(CMAKE_BUILD_TYPE Release)
 ENDIF(NOT CMAKE_BUILD_TYPE)
@@ -15,8 +17,28 @@ message(STATUS "build tests  " ${BUILD_TESTING})
 message(STATUS "build shared " ${BUILD_SHARED_LIBS})
 
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Wall")
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall -g -Wextra")
+
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall -Wextra -Wshadow -Wnon-virtual-dtor -pedantic -g -O0")
 
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/bin")
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/lib")
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/lib")
+
+option(leak_check "set leak_check" 0)
+option(profiling "set profiling" 0)
+option(thread_check "set thread_check" 0)
+
+if(${CMAKE_BUILD_TYPE} STREQUAL Debug AND leak_check)
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer")
+endif()
+
+if(${CMAKE_BUILD_TYPE} STREQUAL Debug AND thread_check)
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=thread")
+endif()
+
+if(${CMAKE_BUILD_TYPE} STREQUAL Debug AND profiling)
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -pg")
+  set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -pg")
+  set(CMAKE_SHARED_LINKER_FLAGS_DEBUG "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -pg")
+  set(CMAKE_STATIC_LINKER_FLAGS_DEBUG "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -pg")
+endif()
--- doxygen.cmake
+++ doxygen.cmake
@@ -14,15 +14,14 @@ if(DOXYGEN_FOUND)
   set(DOXYGEN_OPTIMIZE_OUTPUT_FOR_C "YES")
   set(DOXYGEN_CPP_CLI_SUPPORT "YES")
   set(DOXYGEN_CASE_SENSE_NAMES "NO")
-  set(DOXYGEN_EXCLUDE_PATTERNS "*/build*/*" "*/third_party*/*")
+  set(DOXYGEN_EXCLUDE_PATTERNS "*/build*/*" "*/test*/*" "*/examples*/*" "*/third_party*/*")
   set(DOXYGEN_GENERATE_LATEX "NO")
 
   doxygen_add_docs(doxygen ${CMAKE_CURRENT_SOURCE_DIR} COMMENT "generate doxygen documentation by cmake")
 
-  add_custom_target(generate_documentation ALL
+  add_custom_target(documentation
     COMMAND doxygen "Doxyfile.doxygen"
     WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
     COMMENT "Generate doxygen documentation"
     )
-  set_property(TARGET generate_documentation PROPERTY EXCLUDE_FROM_ALL ON)
 endif()
